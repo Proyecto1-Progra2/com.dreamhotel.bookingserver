77e1ac3b33af96f36e2ecc2d809b5bb068fe354a
Jeffrey0522 <jeffreygranados11@gmail.com>
2025-05-21 13:22:47 -0600

    se guardan imagenes

--- src/main/java/sockets/Client.java
+++ src/main/java/sockets/Client.java
@@ -1,15 +1,14 @@
 package sockets;
 
 import data.HotelData;
+import data.ImageData;
 import data.RoomData;
 import domain.Hotel;
+import domain.Image;
 import domain.Room;
 import utils.Action;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -22,6 +21,7 @@ public class Client extends Thread {
 
     private HotelData hotelData;
     private RoomData roomData;
+    private ImageData imageData;
 
     public Client(Socket socket) throws IOException {
         this.socket = socket;
@@ -30,6 +30,7 @@ public class Client extends Thread {
 
         this.hotelData = new HotelData();
         this.roomData = new RoomData();
+        this.imageData = new ImageData();
     } // constructor
 
     @Override
@@ -37,24 +38,17 @@ public class Client extends Thread {
         try {
             while (true) {
                 this.lectura = this.receive.readLine();
-                //System.out.println(this.lectura);
+                System.out.println(this.lectura);
                 String[] datos = this.lectura.split("-");
                 String accion = datos[0];
-
-
                 switch (accion) {
                     case Action.HOTEL_REGISTER:
                         this.hotelData.insert(new Hotel(datos[1], datos[2], datos[3]));
                         this.send.println(Action.HOTEL_REGISTERED);
                         break;
-
-
                     case Action.HOTEL_NOT_REGISTER:
                         this.send.println(Action.HOTEL_NOT_REGISTERED);
                         break;
-
-
-
                     case Action.HOTEL_LIST:
                         String envioHoteles = Action.HOTEL_LIST;
                         ArrayList<Hotel> hoteles = this.hotelData.findAll();
@@ -78,7 +72,8 @@ public class Client extends Thread {
                         this.send.println(Action.HOTEL_DELETED);
                         break;
                     case Action.ROOM_REGISTER: //el dato 5 es de imagenes, tal tal en el insert de RoomData mandarle un arreglo de bytes con la imagen
-                        this.roomData.insert(new Room(datos[1], datos[2], datos[3], Double.parseDouble(datos[4])));//revisar para meter los datos
+                        this.roomData.insert(new Room(datos[1], datos[2], datos[3], Double.parseDouble(datos[4]), null));//revisar para meter los datos
+                        this.imageData.insert(new Image(datos[5], new File(datos[6])));
                         this.send.println(Action.ROOM_REGISTERED);
                         break;
                     case Action.ROOM_LIST: //revisar
@@ -90,10 +85,10 @@ public class Client extends Thread {
                         this.send.println(envioRooms);
                         break;
                     case Action.ROOM_UPDATE:
-                        Room room = new Room(datos[1], datos[2], datos[3], Double.parseDouble(datos[4]));
-                        int posRoom = this.roomData.buscarPosicion(room.getRoomNumber());
-                        this.roomData.insertPos(room, posRoom);
-                        this.send.println(Action.ROOM_UPDATED);
+                        //Room room = new Room(datos[1], datos[2], datos[3], Double.parseDouble(datos[4]));
+                        //int posRoom = this.roomData.buscarPosicion(room.getRoomNumber());
+                        //this.roomData.insertPos(room, posRoom);
+                        //this.send.println(Action.ROOM_UPDATED);
                         break;
                     default:
                         throw new IllegalStateException("Unexpected value: " + accion);
