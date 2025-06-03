d8cd8b0bde816d8e28b58ed30e3ab9ab522743e1
jimeest16 <jimenasalasthames@gmail.com>
2025-06-03 15:02:57 -0600

    Cambios

--- src/main/java/data/HuespedData.java
+++ src/main/java/data/HuespedData.java
@@ -1,5 +1,8 @@
 package data;
 
+import domain.Host;
+import domain.Receptionist;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,9 +12,15 @@ import java.util.ArrayList;
 public class HuespedData {
 
     private RandomAccessFile raf;
-    private final int TAMANO_REGISTRO = 60;
-    private final int TAMANO_NOMBRE = 30;
-    private final int TAMANO_APELLIDOS = 30;
+    private final int TAMANO_REGISTRO = 176; //total por cantidad de registros
+    private final int TAMANO_ID = 10;
+    private final int TAMANO_NAME = 40;
+    private final int TAMANO_LASTNAME = 40;
+    private final int TAMANO_PHONENUMBER = 16;
+    private final int TAMANO_ADDRESS = 20;
+    private final int TAMANO_EMAIL = 30;
+    private final int TAMANO_COUNTRY = 20;
+
 
     public HuespedData() throws FileNotFoundException {
         this.raf = new RandomAccessFile(new File("huespedes.dat"), "rw");
@@ -35,30 +44,81 @@ public class HuespedData {
         }
         return datos;
     }
-/*
-    public void insert(Resident resident) throws IOException {
+
+    public void insert(Host host) throws IOException {
         //aumentar el tamaño del archivo en 64 bytes (TAMAÑO_REGISTRO)
         raf.setLength(raf.length() + TAMANO_REGISTRO);
         raf.seek(raf.length() - TAMANO_REGISTRO);
         // Se debe transformar el string en un arreglo de bytes para
         // poder escribirlo en el archivo
-        byte nombre[] = toBytes(resident.getName(),TAMANO_NOMBRE);
+        byte nombre[] = toBytes(host.getName(),TAMANO_NAME);
         raf.write(nombre);
-        byte apellidos[]= toBytes(resident.getApellido(),TAMANO_APELLIDOS);
+        byte apellidos[]= toBytes(host.getLastName(),TAMANO_LASTNAME);
         raf.write(apellidos);
+        raf.writeInt(host.getPhoneNumber());//numero de telefono
+        byte address[]= toBytes(host.getAddress(),TAMANO_ADDRESS);
+        raf.write(address);
+        byte email[]= toBytes(host.getEmail(),TAMANO_EMAIL);
+        raf.write(email);
+        byte country[]= toBytes(host.getCountry(),TAMANO_COUNTRY);
+        raf.write(country);
     }
 
-    public ArrayList<Resident> findAll() throws IOException {
+    public ArrayList<Host> findAll() throws IOException {
         int totalRegistros = (int)(raf.length()/TAMANO_REGISTRO);
-        ArrayList<Resident> huespedes = new ArrayList<>();
+        ArrayList<Host> huespedes = new ArrayList<>();
         for(int i=0; i<totalRegistros; i++){
             raf.seek(i*TAMANO_REGISTRO);
-            String nombre = this.readString(TAMANO_NOMBRE,
-                    raf.getFilePointer());
-            String apellidos = this.readString(TAMANO_APELLIDOS,
-                    raf.getFilePointer());
-            huespedes.add(new Resident(nombre, apellidos));
+
+            String id= this.readString(TAMANO_ID,raf.getFilePointer());
+            String name = this.readString(TAMANO_NAME, raf.getFilePointer());
+            String lastName = this.readString(TAMANO_LASTNAME, raf.getFilePointer());
+            int phoneNumber = raf.readInt();
+            String address = this.readString(TAMANO_ADDRESS, raf.getFilePointer());
+            String email = this.readString(TAMANO_EMAIL, raf.getFilePointer());
+            String country = this.readString(TAMANO_COUNTRY, raf.getFilePointer());
+
+            huespedes.add(new Host(id,name,lastName, phoneNumber, address, email, country ));
         }//for
         return huespedes;
-    }*/
+    }
+    public Host hostLogin(String name, String lastName) throws IOException {
+        int totalRegistros = (int) (this.raf.length() / TAMANO_REGISTRO);
+        int numReg = 0;
+        Host host = null;
+
+        while (numReg < totalRegistros) {
+            int posInicioRegistro = numReg * TAMANO_REGISTRO;
+
+            // Leer campos
+            raf.seek(posInicioRegistro);
+            String id = readString(TAMANO_ID, posInicioRegistro).trim();
+
+            int posName = posInicioRegistro + TAMANO_ID;
+            String nameActual = readString(TAMANO_NAME, posName).trim();
+
+            int posLastName = posName + TAMANO_NAME;
+            String lastNameActual = readString(TAMANO_LASTNAME, posLastName).trim();
+
+            int posPhone = posLastName + TAMANO_LASTNAME;
+            raf.seek(posPhone);
+            int phoneNumber = raf.readInt(); // importante leer el int
+
+            int posAddress = posPhone + 4;
+            String address = readString(TAMANO_ADDRESS, posAddress).trim();
+
+            int posEmail = posAddress + TAMANO_ADDRESS;
+            String email = readString(TAMANO_EMAIL, posEmail).trim();
+
+            int posCountry = posAddress + TAMANO_EMAIL;
+            String country = readString(TAMANO_COUNTRY, posCountry).trim();
+
+            if (name.equalsIgnoreCase(nameActual) && lastNameActual.equalsIgnoreCase(lastNameActual)) {
+                host = new Host(id, nameActual, lastNameActual, phoneNumber, address, email, country);
+            }
+
+            numReg++;
+        }
+        return host;
+    }
 }
